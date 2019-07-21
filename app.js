dcc66588c092ee61c3f9d86ca6072f53b8a9623d
dannydenenberg <dannydenenberg@gmail.com>
2019-07-21 12:59:04 -0500

    pass

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const mongodb = require("mongodb"); // load mongodb
 
 const crypto = require("crypto"); // for hashing passwords
 
-const port = process.env.PORT;
+const port = process.env.PORT || 4567;
 // const port = 4567;
 
 const app = express(); // instantiate express
@@ -32,7 +32,7 @@ mongodb.MongoClient.connect(uri, (err, db) => {
   app.get("/:user", (req, res) => {
     collection.find({ user: req.params.user }).toArray((err, docs) => {
       if (err) {
-        res.send("An error occured in getting the user info.");
+        res.send({ response: "An error occured in getting the user info." });
       } else {
         // there were matches (there are users with that username)
         if (docs.length > 0) {
@@ -41,7 +41,9 @@ mongodb.MongoClient.connect(uri, (err, db) => {
 
           // if there was no password sent in the query of the url (after the `?`)
           if (!req.query.pass) {
-            res.send("There was no password associated with the GET req.");
+            res.send({
+              response: "There was no password associated with the GET req."
+            });
           }
 
           // the password sent in the GET req.
@@ -88,21 +90,54 @@ mongodb.MongoClient.connect(uri, (err, db) => {
 
   // this doesn't create a new user but rather updates an existing one by the user name
   // base route
+  // a request looks like this: `https://nodeserver.com/username23?pass=12345` plus the associated JSON data
   app.put("/:user", (req, res) => {
-    // hash the password
-    req.body.pass = hash(req.body.pass);
+    // get the user, compare the hashed password sent in the query to the one in the database, if they match, then update the data
+    collection.find({ user: req.params.user }).toArray((err, docs) => {
+      if (err) {
+        res.send({ response: "An error occured in getting the user info." });
+      } else {
+        // there were matches (there are users with that username)
+        if (docs.length > 0) {
+          // get the first users password (each user should have a unique username)
+          let hashedPasswordInDatabase = docs[0].pass;
 
-    collection.updateOne(
-      { user: req.params.user }, // if the username is the same, update the user
-      { $set: { ...req.body, user: req.params.user } },
-      (err, r) => {
-        if (err) {
-          res.send("An error occured");
+          // if there was no password sent in the query of the url (after the `?`)
+          if (!req.query.pass) {
+            res.send({
+              response: "There was no password associated with the GET req."
+            });
+          }
+
+          // the password sent in the GET req.
+          const hashedQueryPassword = hash(`${req.query.pass}`); // enclose in template to make sure it is a string (so if a password is a number, it treats it as a string)
+
+          // if the password that was sent with the get request matches the user's password
+          if (hashedPasswordInDatabase === hashedQueryPassword) {
+            // update user info in database
+
+            // hash the password for storing
+            req.body.pass = hash(req.body.pass);
+            collection.updateOne(
+              { user: req.params.user }, // if the username is the same, update the user
+              { $set: { ...req.body, user: req.params.user } },
+              (err, r) => {
+                if (err) {
+                  console.log("AN error occurred in updating information");
+                }
+              }
+            );
+            res.send({ correctPassword: true });
+          } else {
+            // otherwise, the password was wrong
+            res.send({ correctPassword: false });
+          }
         } else {
-          res.send("All well");
+          res.send("There were no users with that name found. ");
+          console.log("There were no users with that name found. ");
         }
       }
-    );
+    });
   });
 
   // listen for requests, the process.env.PORT is needed because
